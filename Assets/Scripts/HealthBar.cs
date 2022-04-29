c74d0ab20a73d47c59a56bcf9b95e4b0a5f187ac
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2022-04-29 11:49:22 -0400

    Added Damage & updated Player Stats

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace IH
+{
+    public class HealthBar : MonoBehaviour
+    {
+        private Slider slider;
+
+        private void Start()
+        {
+            slider = GetComponent<Slider>();
+        }
+
+        public void SetMaxHealth(int maxHealth)
+        {
+            slider.maxValue = maxHealth;
+            slider.value = maxHealth;
+        }
+
+        public void SetCurrentHealth(int currentHealth)
+        {
+            slider.value = currentHealth;
+        }
+       
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace IH
+{
+    public class PlayerStats : MonoBehaviour
+    {
+
+        public int healthLevel = 10;
+        public int maxHealth;
+        public int currentHealth; 
+
+        public HealthBar healthBar;
+
+        AnimationHandler animatorHandler;
+
+        private void Awake()
+        {
+            animatorHandler = GetComponentInChildren<AnimationHandler>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            maxHealth = SetMaxHealthFromHealthLevel();
+            currentHealth = maxHealth; 
+            healthBar.SetMaxHealth(maxHealth);
+        }
+
+        public int SetMaxHealthFromHealthLevel()
+        {
+            maxHealth = healthLevel * 10; 
+            return maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHealth = currentHealth - damage;
+
+            healthBar.SetCurrentHealth(currentHealth);
+            animatorHandler.PlayTargetAnimations("Damage_01", true); 
+
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                animatorHandler.PlayTargetAnimations("Dead_01", true); 
+            }
+        }
+
+      
+    }
+}
+
